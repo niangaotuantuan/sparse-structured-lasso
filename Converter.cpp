8ada196959f676d218599f645eb7e824fbb58907
Yanran Li <summer.li@SummerLi.local>
2015-05-19 13:25:24 +0800

    add converter.cpp

---
+++ Converter.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include "Grid.h"
+#include "mpi.h"
+#include "Definition.h"
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+  MPI_Init(&argc, &argv);
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (argc < 3) {
+    cerr << "./converter <infile> <outfile>\n";
+    MPI_Abort(MPI_COMM_WORLD, -1);
+  }
+  string fnameIn(argv[1]);
+  string fnameOut(argv[2]);
+
+  Grid grid;
+  if (grid.GetRank() == 0)
+    grid.Convert(fnameIn);
+  grid.Save(fnameOut);
+}
